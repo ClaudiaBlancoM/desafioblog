9b52da97ec95a224ac74fce1ece6ff29d9d1cc12
ClaudiaBlancoM <cabm0626@gmail.com>
2022-05-13 18:54:32 -0400

    cambio

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_001731) do
-  create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_001717) do
 end
