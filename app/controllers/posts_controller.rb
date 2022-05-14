8532f320369c7a1f3dd932ce3404788dc05a1dd1
ClaudiaBlancoM <cabm0626@gmail.com>
2022-05-13 23:17:34 -0400

    second commit 20220513

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,21 @@
 class PostsController < ApplicationController
     before_action :set_post, only: %i[ show edit update destroy ]
+
     def index
         @posts = Post.all
     end
+
     def show
         @post = Post.find(params[:id])
     end
+
     def new
         @post = Post.new
     end
+
     def create
         @post = Post.new(post_params)
+       
         respond_to do |format|
           if @post.save
             format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
@@ -21,6 +26,7 @@ class PostsController < ApplicationController
           end
         end
     end
+
     def update
         if @post.update(params)
             redirect_to @post, notice: "Post was successfully updated."
@@ -28,18 +34,25 @@ class PostsController < ApplicationController
             render :edit, status: :unprocessable_entity
         end
     end
+
     def destroy
         @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_url, notice: "Post was successfully destroyed."
     end
+    
     private
+           
         # Use callbacks to share common setup or constraints between actions.
         def set_post
             @post = Post.find(params[:id])
         end
+    
         # Only allow a list of trusted parameters through.
         def post_params
             params.require(:post).permit(:title, :description, :author)
         end
-end
+    
+        
+      
+end     
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_231154) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_14_005808) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
